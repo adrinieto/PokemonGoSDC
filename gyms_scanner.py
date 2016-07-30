5aef6b2dac81b11099be79586009b8cfa1bd48e4
Adrián Nieto <adrian.nieto7@gmail.com>
2016-07-30 18:28:44 +0200

    Added a database

--- gyms_scanner.py
+++ gyms_scanner.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 import json
 from collections import Counter, namedtuple
 from datetime import datetime
@@ -6,10 +7,14 @@ from time import sleep
 
 import csv
 
+from peewee import InsertQuery
+
+import models
+from models import create_tables, init_database
 from utils import setup_logging, setup_api, timestamp_to_strftime
 
 TEAMS = {
-    0: 'neutral',
+    0: 'Neutral',
     1: 'Mystic',
     2: 'Valor',
     3: 'Instint'
@@ -62,71 +67,126 @@ def get_data_from_server(gyms):
     return gym_details
 
 
-def gyms_by_team(gym_details):
-    teams = []
-    for gym_detail in gym_details:
-        if 'owned_by_team' not in gym_detail['gym_state']['fort_data']:
-            teams.append(0)
-            continue
-        team = int(gym_detail['gym_state']['fort_data']['owned_by_team'])
-        teams.append(team)
+def parse_and_insert_to_database(gym_details):
+    create_tables()
+    now = datetime.now()
 
-    team_counter = Counter(teams)
+    gyms = {}
+    trainers = {}
+    pokemons = {}
+    gym_members = []
+
+    for gym_detail in gym_details:
+        gym_data = gym_detail['gym_state']['fort_data']
+        gym_id = gym_data['id']
+        gyms[gym_id] = {
+            'id': gym_id,
+            'name': gym_detail['name'],
+            'description': gym_detail.get('description', ''),
+            'team_id': gym_data.get('owned_by_team', models.Gym.UNCONTESTED),
+            'guard_pokemon_id': gym_data.get('guard_pokemon_id', 0),
+            'gym_points': gym_data.get('gym_points', 0),
+            'is_in_battle': gym_data.get('is_in_battle', False),
+            'enabled': gym_data['enabled'],
+            'latitude': gym_data['latitude'],
+            'longitude': gym_data['longitude'],
+            'last_modified': datetime.utcfromtimestamp(
+                gym_data['last_modified_timestamp_ms'] / 1000.0),
+            'last_checked': now,
+        }
+
+        memberships = gym_detail['gym_state'].get('memberships', [])
+
+        for member in memberships:
+            pokemon_data = member['pokemon_data']
+            trainer_data = member['trainer_public_profile']
+
+            trainer_name = trainer_data['name']
+            trainers[trainer_name] = {
+                'name': trainer_name,
+                'level': trainer_data['level'],
+                'last_checked': now
+            }
+
+            pokemon_id = pokemon_data['id']
+            pokemons[pokemon_id] = {
+                'id': pokemon_id,
+                'owner': pokemon_data['owner_name'],
+                'pokemon_id': pokemon_data['pokemon_id'],
+                'cp': pokemon_data['cp'],
+                'last_checked': now
+            }
+
+            gym_members.append({
+                'trainer': trainer_name,
+                'gym': gym_id,
+                'pokemon': pokemon_id
+            })
+
+    InsertQuery(models.Gym, rows=gyms.values()).upsert().execute()
+    InsertQuery(models.Trainer, rows=trainers.values()).upsert().execute()
+    InsertQuery(models.Pokemon, rows=pokemons.values()).upsert().execute()
+    InsertQuery(models.GymMember, rows=gym_members).upsert().execute()
+
+
+def print_gyms_by_team():
+    gyms = models.Gym.select()
+    team_counter = Counter([gym.team_id for gym in gyms])
     total_gyms = sum(team_counter.values())
-    print "Total gyms: %s" % total_gyms
+    print
+    print "Gimnasios por equipos"
+    print "-" * 30
+    print "Número de gimnasios: {}".format(total_gyms)
     for team, gyms_owned in team_counter.iteritems():
-        print "{:10} {:2}  ({:.1f}%)".format(TEAMS[team], gyms_owned, gyms_owned / float(total_gyms) * 100)
+        print "{:10} {:5}  ({:.1f}%)".format(TEAMS[team], gyms_owned, gyms_owned / float(total_gyms) * 100)
+
+
+def print_top_trainers():
+    top_trainers = models.Trainer.sorted_by_level()[:10]
+    print
+    print "TOP 10 trainers (by level)"
+    print "-" * 30
+    print "{:20} {:7}".format("TRAINER", "LEVEL")
+    for trainer in top_trainers:
+        print "{:20} {:<7}".format(trainer.name, trainer.level)
+
+
+def print_top_gyms_owned():
+    top_gyms_owned = models.Trainer.top_gyms_owned()[:10]
+    print
+    print "Gimnasios por entrenador"
+    print "-" * 30
+    print "{:20} {:7} {:5}".format("TRAINER", "LEVEL", "GYMS OWNED")
+    for trainer in top_gyms_owned:
+        print "{:20} {:<7} {:5}".format(
+            trainer.name, trainer.level, len(trainer.gyms_membership))
+
+
+def print_gyms_details():
+    print
+    for gym in models.Gym.select():
+        print "-", gym.name
+        print "  Controlled by: {}".format(gym.team)
+        print "  {} points (level {})".format(gym.gym_points, gym.level)
+        print "  {} trainers:".format(len(gym.members))
+        for member in gym.members:
+            print "    - {:4} CP ({:15} level {:2})".format(
+                member.pokemon.cp, member.trainer.name, member.trainer.level)
 
 
 def main():
-    gyms = read_gyms_from_csv('gyms_santiago.csv')
-    gym_details = get_data_from_server(gyms)
+    gyms_dict = read_gyms_from_csv('gyms_santiago.csv')
+    gym_details = get_data_from_server(gyms_dict)
     save_to_json(gym_details)
 
     # gym_details = read_data_from_json('gym_details.json')
 
-    gyms_by_team(gym_details)
+    parse_and_insert_to_database(gym_details)
 
-    gym_members_counter = Counter()
-
-    trainers_level = {}
-    for gym_detail in gym_details:
-        gym_data = gym_detail['gym_state']['fort_data']
-        owned_by_team = int(gym_data['owned_by_team']) if 'owned_by_team' in gym_data else 0
-        gym_members = gym_detail['gym_state']['memberships'] if 'owned_by_team' in gym_data else []
-        is_in_battle = gym_data['is_in_battle'] if 'is_in_battle' in gym_data else False
-        last_modified = timestamp_to_strftime(gym_data['last_modified_timestamp_ms']) if 'last_modified_timestamp_ms' in gym_data else 0
-        gym_points = gym_data['gym_points'] if 'gym_points' in gym_data else 0
-        print u"{name}: level {level} ({points} points) team {team} (last interaction at {last_modified} {in_battle}".format(
-            name=gym_detail['name'],
-            level=len(gym_members),
-            points=gym_points,
-            team=TEAMS[owned_by_team],
-            last_modified=last_modified,
-            in_battle='' if not is_in_battle else 'IN BATTLE!!'
-        )
-        member_names = []
-        for member in gym_members:
-            pokemon_data = member['pokemon_data']
-            trainer_data = member['trainer_public_profile']
-            trainer_name = trainer_data['name']
-            trainer_level = trainer_data['level']
-
-            trainers_level[trainer_name] = trainer_level
-
-            member_names.append(trainer_name)
-            print "\t{owner_name:15}  {level:2}{cp:8} CP ".format(
-                owner_name=trainer_name,
-                level=trainer_level,
-                cp=pokemon_data['cp'],
-            )
-        gym_members_counter.update(member_names)
-
-    for trainer_name, count in gym_members_counter.most_common(15):
-        print "{:15} (level {:2}): {:2} gyms".format(trainer_name, trainers_level[trainer_name], count)
-
-    trainers = sorted(trainers_level.items(), key=lambda x: x[1], reverse=True)
-    pprint(trainers[:10])
+    print_gyms_details()
+    print_gyms_by_team()
+    print_top_trainers()
+    print_top_gyms_owned()
 
 
 if __name__ == '__main__':
---
+++ models.py
@@ -0,0 +1,112 @@
+# coding: utf-8
+import logging
+
+from peewee import Model, SqliteDatabase, CharField, IntegerField, BooleanField, DoubleField, DateTimeField, \
+    ForeignKeyField, fn, SQL
+
+log = logging.getLogger(__name__)
+
+DATABASE = "database.sqlite"
+db = None
+
+
+def init_database():
+    global db
+    if db is not None:
+        return db
+
+    db = SqliteDatabase(DATABASE)
+    log.info('Connecting to local SQLite database.')
+
+    return db
+
+
+def create_tables():
+    init_database()
+    db.connect()
+    tables = [Trainer, Pokemon, Gym, GymMember]
+    db.drop_tables(tables)
+    db.create_tables(tables, safe=True)
+    db.close()
+
+
+class BaseModel(Model):
+    class Meta:
+        database = init_database()
+
+
+class Trainer(BaseModel):
+    name = CharField(primary_key=True, max_length=50)
+    level = IntegerField()
+    last_checked = DateTimeField()
+
+    @classmethod
+    def sorted_by_level(cls):
+        return list(Trainer.select()
+                    .order_by(Trainer.level.desc()))
+
+    @classmethod
+    def top_gyms_owned(cls):
+        return Trainer.select(Trainer, GymMember).join(GymMember).group_by(GymMember.trainer).order_by(
+            fn.COUNT(GymMember.trainer).desc())
+
+    def __repr__(self):
+        return "Trainer(name={}, level={}))".format(self.name, self.level)
+
+
+class Pokemon(BaseModel):
+    id = CharField(primary_key=True, max_length=50)
+    owner = ForeignKeyField(Trainer, related_name='pokemons')
+    pokemon_id = IntegerField()
+    cp = IntegerField()
+    last_checked = DateTimeField()
+
+    def __repr__(self):
+        return "Pokemon(id={}, owner={}))".format(self.id, self.owner)
+
+
+class Gym(BaseModel):
+    TEAMS = ['Uncontested', 'Mystic', 'Valor', 'Instint']
+    UNCONTESTED = 0
+    TEAM_MYSTIC = 1
+    TEAM_VALOR = 2
+    TEAM_INSTINCT = 3
+
+    id = CharField(primary_key=True, max_length=50)
+    name = CharField(max_length=50)
+    description = CharField(max_length=400)
+    team_id = IntegerField()
+    guard_pokemon_id = IntegerField()
+    gym_points = IntegerField()
+    is_in_battle = BooleanField()
+    enabled = BooleanField()
+    latitude = DoubleField()
+    longitude = DoubleField()
+    last_modified = DateTimeField()
+    last_checked = DateTimeField()
+
+    @property
+    def team(self):
+        return Gym.TEAMS[self.team_id]
+
+    @property
+    def level(self):
+        points_per_level = [2000, 4000, 8000, 12000, 16000, 20000, 30000, 40000, 50000]
+        level = 1
+        while self.gym_points >= points_per_level[level -1]:
+            level += 1
+        return level
+
+    def __repr__(self):
+        return "Gym(id={}, name={}, team_id={}, gym_points={}, last_modified={}, members_count={}))".format(
+            self.id, self.name.encode('utf-8'), self.team_id, self.gym_points, self.last_modified, len(self.members))
+
+
+class GymMember(BaseModel):
+    trainer = ForeignKeyField(Trainer, related_name='gyms_membership')
+    gym = ForeignKeyField(Gym, related_name='members')
+    pokemon = ForeignKeyField(Pokemon)
+
+    def __repr__(self):
+        return str(self.__dict__)
+        # return "GymMember(trainer={}, gym={}, pokemon={}))".format(self.trainer, self.gym, self.pokemon)
