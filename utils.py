3118b4fdf0940eb1aaf456f0de0c04724b538fe4
Adrián Nieto <adrian.nieto7@gmail.com>
2016-09-13 00:19:38 +0200

    Add libencrypt for multiple platforms to the repo

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import logging
+import os
+import platform
+import sys
 from datetime import datetime
 
 from pgoapi import pgoapi
 
-from config import ENCRYPT_LIB_PATH
+log = logging.getLogger(__name__)
 
 
 def setup_logging():
@@ -26,10 +29,57 @@ def setup_api(position, auth_service, username, password):
 
     api.set_authentication(provider=auth_service, username=username, password=password)
 
-    api.activate_signature(ENCRYPT_LIB_PATH)
+    api.activate_signature(get_encryption_lib_path())
 
     return api
 
 
 def timestamp_to_strftime(timestamp):
     return datetime.fromtimestamp(int(timestamp) / 1000).strftime('%Y-%m-%d %H:%M:%S')
+
+
+def get_encryption_lib_path():
+    # win32 doesn't mean necessarily 32 bits
+    if sys.platform == "win32" or sys.platform == "cygwin":
+        if platform.architecture()[0] == '64bit':
+            lib_name = "encrypt64bit.dll"
+        else:
+            lib_name = "encrypt32bit.dll"
+
+    elif sys.platform == "darwin":
+        lib_name = "libencrypt-osx-64.so"
+
+    elif os.uname()[4].startswith("arm") and platform.architecture()[0] == '32bit':
+        lib_name = "libencrypt-linux-arm-32.so"
+
+    elif os.uname()[4].startswith("aarch64") and platform.architecture()[0] == '64bit':
+        lib_name = "libencrypt-linux-arm-64.so"
+
+    elif sys.platform.startswith('linux'):
+        if "centos" in platform.platform():
+            if platform.architecture()[0] == '64bit':
+                lib_name = "libencrypt-centos-x86-64.so"
+            else:
+                lib_name = "libencrypt-linux-x86-32.so"
+        else:
+            if platform.architecture()[0] == '64bit':
+                lib_name = "libencrypt-linux-x86-64.so"
+            else:
+                lib_name = "libencrypt-linux-x86-32.so"
+
+    elif sys.platform.startswith('freebsd'):
+        lib_name = "libencrypt-freebsd-64.so"
+
+    else:
+        err = "Unexpected/unsupported platform '{}'".format(
+            sys.platform)
+        log.error(err)
+        raise Exception(err)
+
+    lib_path = os.path.join(os.path.dirname(__file__), "libencrypt", lib_name)
+
+    if not os.path.isfile(lib_path):
+        err = "Could not find {} encryption library {}".format(sys.platform, lib_path)
+        log.error(err)
+        raise Exception(err)
+    return lib_path
